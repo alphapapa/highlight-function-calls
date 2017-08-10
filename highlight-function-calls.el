c54ea6111b58c27d6fb42c35a737ab8e6a345820
Adam Porter <adam@alphapapa.net>
2017-08-09 19:25:55 -0500

    Also match null

--- highlight-function-calls.el
+++ highlight-function-calls.el
@@ -121,7 +121,7 @@
             (goto-char (match-end 0))
             (setq highlight-function-calls--face-name
                   (pcase match
-                    ((and 'not (guard highlight-function-calls-not)) 'highlight-function-calls--not-face)
+                    ((and (or 'not 'null) (guard highlight-function-calls-not)) 'highlight-function-calls--not-face)
                     (_ 'highlight-function-calls--face)))
             (throw 'highlight-function-calls--matcher t)))))
     nil))
